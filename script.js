9aa5d3c9630934c7823eedced28f8752aa4d6f2c
Raul Miranda <raul.oliveira.miran1@managementsolutions.com>
2023-11-25 21:26:30 -0300

    Adição de atendimento e triagem

---
+++ script.js
@@ -0,0 +1,31 @@
+let tabsAtendimento = document.querySelectorAll(".tabsAtendimento h3");
+let tabContents = document.querySelectorAll(".tab-content div");
+
+tabsAtendimento.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabContents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    tabsAtendimento.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tabContents[index].classList.add("active");
+    tabsAtendimento[index].classList.add("active");
+  });
+});
+
+let tabsTriagem = document.querySelectorAll(".tabsTriagem h3");
+// let tabContents = document.querySelectorAll(".tab-content div");
+
+tabsTriagem.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabContents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    tabsTriagem.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tabContents[index].classList.add("active");
+    tabsTriagem[index].classList.add("active");
+  });
+});
