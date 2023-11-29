1c9e77f8dda49c75620601ec262a42fdd4e488c2
Raul Miranda <raul.oliveira.miran1@managementsolutions.com>
2023-11-29 17:16:28 -0300

    Iniciar mapa de calor brasil

--- script.js
+++ script.js
@@ -297,5 +297,25 @@ var NormativosPublicados = document.getElementById('NormativosPublicados').getCo
       },
     });
 
+    
+    const description = document.querySelector(".tooltip");
 
+		document.querySelectorAll('path').forEach((el) =>
+			el.addEventListener('mouseover', (event) => {
+				event.target.className = ("enabled");
+				description.classList.add("active");
+				description.innerHTML = event.target.id;
+			})
 
+		);
+
+		document.querySelectorAll('path').forEach((el) =>
+			el.addEventListener("mouseout", () => {
+				description.classList.remove("active");
+			})
+		);
+
+		document.onmousemove = function (e) {
+			description.style.left = e.pageX + "px";
+			description.style.top = (e.pageY - 70) + "px";
+		}
