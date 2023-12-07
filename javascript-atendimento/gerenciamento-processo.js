0e86f1844a7e62883d05cfcf10f6413bf88c7dcf
matheus shinji <matheus.fukuyama@msbrazil.com>
2023-12-07 09:34:53 -0300

    botao abrir das telas gestao de processos e centralizador concertados

--- javascript-atendimento/gerenciamento-processo.js
+++ javascript-atendimento/gerenciamento-processo.js
@@ -20,12 +20,11 @@ atendimentoGestaoFehcarPopUp.addEventListener("click", function() {
 })
 
 const atendimendimentoGerenciamentoAbrirBtn = document.getElementById("atendimento-gestao-abrir-btn")
-const telaGestao = document.getElementById("centralizador-lista")
+const telaGestao = document.getElementById("gerenciamento-processo-lista")
 const telaGestaoAbrir = document.getElementsByClassName("atendimendimento-gestao-processo-abrir")[0]
 const atendimentoGestaoAbrirFechar = document.getElementById("atendimento-gestao-processo-btn-fechar")
 
 atendimendimentoGerenciamentoAbrirBtn.addEventListener("click", function() {
-    console.log("aqui")
     telaGestao.style.display = "none"
     telaGestaoAbrir.style.display= "flex"
 })
